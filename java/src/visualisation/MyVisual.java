5e5bdd293d05906158df4802641b7b433bc735a9
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2023-05-03 00:09:43 +0100

    Add Primavera song

--- java/src/visualisation/MyVisual.java
+++ java/src/visualisation/MyVisual.java
@@ -20,10 +20,7 @@ public class MyVisual extends Visual {
         startMinim();
 
         // Call loadAudio to load an audio file to process
-        // loadAudio("heroplanet.mp3");
-
-        // Call this instead to read audio from the microphone
-        startListening();
+        loadAudio("songs\\primavera_short.mp3");
 
         colorMode(HSB, 360, 100, 100);
         layers = new ArrayList<Layer>();
