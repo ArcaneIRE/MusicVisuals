46fa7c6709da7545da83a1138f6b096e647ab21d
ArcaneIRE <44246360+ArcaneIRE@users.noreply.github.com>
2023-05-03 00:02:37 +0100

    Optimize rendering with PGraphics

--- java/src/visualisation/Land.java
+++ java/src/visualisation/Land.java
@@ -1,9 +1,11 @@
 package visualisation;
 
 import processing.core.PApplet;
+import processing.core.PGraphics;
 
 public class Land extends RenderObject {
     float heightMap[];
+    PGraphics buffer;
     Color color;
 
     public Land(PApplet p, float x, float y, Color color) {
@@ -11,15 +13,26 @@ public class Land extends RenderObject {
         this.color = color;
         this.heightMap = new float[p.width];
 
+        setup();
+    }
+
+    private void setup() {
         generateHeightMap();
+
+        buffer = p.createGraphics(p.width, p.height);
+        buffer.beginDraw();
+        buffer.colorMode(PApplet.HSB, 360, 100, 100);
+        buffer.stroke(color.hue, color.sat, color.bri);
+        buffer.fill(color.hue, color.sat, color.bri);
+        buffer.rect(0, pos.y, p.width, p.height - pos.y);
+        for (int i = 0; i < heightMap.length; i++) {
+            buffer.line(i, pos.y - heightMap[i], i, pos.y);
+        }
+        buffer.endDraw();
     }
 
     public void render() {
-        p.stroke(color.hue, color.sat, color.bri);
-        p.strokeWeight(1);
-        for (int i = 0; i < heightMap.length; i++) {
-            p.line(i, pos.y + heightMap[i], i, p.height);
-        }
+        p.image(buffer, 0, 0);
     }
 
     public void update() {
