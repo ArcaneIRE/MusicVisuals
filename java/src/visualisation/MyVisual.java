e9efe986d947a3b58d2306dfe7baa0e3052fbf38
David Corcoran <44246360+ArcaneIRE@users.noreply.github.com>
2023-05-02 21:07:39 +0100

    Create example scene

--- java/src/visualisation/MyVisual.java
+++ java/src/visualisation/MyVisual.java
@@ -25,7 +25,16 @@ public class MyVisual extends Visual {
         // Call this instead to read audio from the microphone
         startListening();
 
+        colorMode(HSB, 360, 100, 100);
         layers = new ArrayList<Layer>();
+
+        layers.add(new Layer(this));
+        layers.add(new Layer(this));
+        layers.add(new Layer(this));
+
+        layers.get(0).renderObjects.add(new Land(this, 0, 150, new Color(130, 80, 70)));
+        layers.get(1).renderObjects.add(new Land(this, 0, 200, new Color(130, 80, 60)));
+        layers.get(2).renderObjects.add(new Land(this, 0, 250, new Color(130, 80, 50)));
     }
 
     public void keyPressed() {
@@ -36,7 +45,7 @@ public class MyVisual extends Visual {
     }
 
     public void draw() {
-        background(0);
+        background(200, 60, 100);
         try {
             // Call this if you want to use FFT data
             calculateFFT();
