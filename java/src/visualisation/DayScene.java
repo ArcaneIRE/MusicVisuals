fb9ac6659ec292379e99de1c413b660f1c8ec7c7
KRD <kaimorciniec@gmail.com>
2023-05-04 03:39:12 +0100

    Create DayScene

---
+++ java/src/visualisation/DayScene.java
@@ -0,0 +1,95 @@
+package visualisation;
+
+import java.util.ArrayList;
+import processing.core.*;
+
+public class DayScene {
+    private MyVisual mv;
+    private ArrayList<Layer> layers;
+    private Sun sun;
+    private Bird[] flock;
+    private River river;
+
+    public DayScene(MyVisual mv) {
+        this.mv = mv;
+        setup();
+    }
+
+    private void setup() {
+        layers = new ArrayList<Layer>();
+
+        mv.currentBackgroundColor = mv.color(200, 60, 100);
+
+        layers = new ArrayList<Layer>();
+
+        layers.add(new Layer(mv));
+        layers.get(0).renderObjects.add(new Land(mv, 0, 13 * (mv.height / 20), new Color(130, 68, 70), 0));
+        layers.add(new Layer(mv));
+        layers.get(1).renderObjects.add(new Land(mv, 0, 14 * (mv.height / 20), new Color(130, 71, 60), 1));
+        layers.add(new Layer(mv));
+        layers.get(2).renderObjects.add(new Land(mv, 0, 15 * (mv.height / 20), new Color(130, 74, 50), 2));
+        layers.add(new Layer(mv));
+        layers.get(3).renderObjects.add(new Land(mv, 0, 16 * (mv.height / 20), new Color(130, 77, 40), 3));
+        layers.add(new Layer(mv));
+        layers.get(4).renderObjects.add(new Land(mv, 0, 17 * (mv.height / 20), new Color(130, 80, 30), 4));
+
+        sun = new Sun(mv, mv.width - 300, mv.height / 10, 80, 24);
+        river = new River(mv, 0, 18 * (mv.height / 19), 40, 50);
+    }
+
+    private void updateBackgroundColor(float elapsedTime) {
+        if (elapsedTime >= mv.sunsetStartTime && elapsedTime <= mv.sunsetStartTime + mv.sunsetDuration) {
+            float progress = (elapsedTime - mv.sunsetStartTime) / mv.sunsetDuration;
+            int startColor = mv.color(200, 60, 100);
+            int endColor = mv.color(340, 10, 10);
+            mv.currentBackgroundColor = mv.lerpColor(startColor, endColor, progress);
+        }
+    }
+
+    private void updateSunPosition(float elapsedTime) {
+        if (elapsedTime >= mv.sunsetStartTime && elapsedTime <= mv.sunsetStartTime + mv.sunsetDuration) {
+            float progress = (elapsedTime - mv.sunsetStartTime) / mv.sunsetDuration;
+            int startY = mv.height / 10;
+            int endY = mv.height - 80 * 2;
+            sun.y = (int) PApplet.lerp(startY, endY, progress);
+        }
+    }
+
+    private void spawnBirds(float elapsedTime) {
+        if (elapsedTime > 0f && flock == null) {
+            flock = new Bird[5];
+            flock[0] = new Bird(mv, -10, 200);
+            flock[1] = new Bird(mv, -40, 175);
+            flock[2] = new Bird(mv, -40, 225);
+            flock[3] = new Bird(mv, -70, 150);
+            flock[4] = new Bird(mv, -70, 250);
+        }
+    }
+
+    private void renderBirds(float elapsedTime) {
+        if (elapsedTime > 0f) {
+            for (Bird bird : flock) {
+                bird.render();
+                bird.update();
+            }
+        }
+    }
+
+    public void render(float elapsedTime) {
+        updateBackgroundColor(elapsedTime);
+        mv.background(mv.currentBackgroundColor);
+        updateSunPosition(elapsedTime);
+        sun.render();
+
+        for (Layer layer : layers) {
+            layer.draw();
+            layer.update();
+        }
+
+        spawnBirds(elapsedTime);
+        renderBirds(elapsedTime);
+
+        river.render();
+        river.update();
+    }
+}
--- java/src/visualisation/MyVisual.java
+++ java/src/visualisation/MyVisual.java
@@ -1,19 +1,14 @@
 package visualisation;
 
 import ie.tudublin.*;
-import java.util.ArrayList;
 
 public class MyVisual extends Visual {
-    private ArrayList<Layer> layers;
-    Sun sun;
-    Bird flock[];
-    River river;
-
     int currentBackgroundColor;
+    DayScene dayScene;
 
     float elapsedTime;
-    float sunsetStartTime = 128.0f;
-    float sunsetDuration = 27.0f;
+    float sunsetStartTime = 0.0f;
+    float sunsetDuration = 19.0f;
 
     public void settings() {
         size(1024, 500);
@@ -30,72 +25,15 @@ public class MyVisual extends Visual {
         getAudioPlayer().play();
 
         colorMode(HSB, 360, 100, 100);
-        currentBackgroundColor = color(200, 60, 100);
+        dayScene = new DayScene(this);
 
-        layers = new ArrayList<Layer>();
-
-        layers.add(new Layer(this));
-        layers.get(0).renderObjects.add(new Land(this, 0, 13 * (height / 20), new Color(130, 68, 70), 0));
-        layers.add(new Layer(this));
-        layers.get(1).renderObjects.add(new Land(this, 0, 14 * (height / 20), new Color(130, 71, 60), 1));
-        layers.add(new Layer(this));
-        layers.get(2).renderObjects.add(new Land(this, 0, 15 * (height / 20), new Color(130, 74, 50), 2));
-        layers.add(new Layer(this));
-        layers.get(3).renderObjects.add(new Land(this, 0, 16 * (height / 20), new Color(130, 77, 40), 3));
-        layers.add(new Layer(this));
-        layers.get(4).renderObjects.add(new Land(this, 0, 17 * (height / 20), new Color(130, 80, 30), 4));
-
-        sun = new Sun(this, width - 300, height / 10, 80, 24);
-
-        river = new River(this, 0, 18 * (height / 19), 40, 50);
     }
 
-    public void keyPressed() {
-    }
-
-    public void updateBackgroundColor(float elapsedTime) {
-        if (elapsedTime >= sunsetStartTime && elapsedTime <= sunsetStartTime + sunsetDuration) {
-            float progress = (elapsedTime - sunsetStartTime) / sunsetDuration;
-            int startColor = color(200, 60, 100);
-            int endColor = color(340, 10, 10);
-            currentBackgroundColor = lerpColor(startColor, endColor, progress);
-        }
-    }
-
-    public void updateSunPosition(float elapsedTime) {
-        if (elapsedTime >= sunsetStartTime && elapsedTime <= sunsetStartTime + sunsetDuration) {
-            float progress = (elapsedTime - sunsetStartTime) / sunsetDuration;
-            int startY = height / 10;
-            int endY = height - 80 * 2;
-            sun.y = (int) lerp(startY, endY, progress);
-        }
-    }
-
-    public void spawnBirds(float elapsedTime) {
-        if (elapsedTime > 0f && flock == null) {
-            flock = new Bird[5];
-            flock[0] = new Bird(this, -10, 200);
-            flock[1] = new Bird(this, -40, 175);
-            flock[2] = new Bird(this, -40, 225);
-            flock[3] = new Bird(this, -70, 150);
-            flock[4] = new Bird(this, -70, 250);
-        }
-    }
-
-    public void renderBirds(float elapsedTime) {
-        if (elapsedTime > 0f) {
-            for (Bird bird : flock) {
-                bird.render();
-                bird.update();
-            }
-        }
+    public void keyPresed() {
     }
 
     public void draw() {
         elapsedTime = getAudioPlayer().position() / 1000.0f; // Get elapsed time in seconds
-
-        spawnBirds(elapsedTime);
-        updateBackgroundColor(elapsedTime);
         background(currentBackgroundColor);
         try {
             // Call this if you want to use FFT data
@@ -109,17 +47,9 @@ public class MyVisual extends Visual {
         // Call this is you want to get the average amplitude
         calculateAverageAmplitude();
 
-        updateSunPosition(elapsedTime);
-        sun.render();
-
-        for (Layer layer : layers) {
-            layer.draw();
-            layer.update();
+        if (elapsedTime <= sunsetStartTime + sunsetDuration) {
+            dayScene.render(elapsedTime);
         }
 
-        renderBirds(elapsedTime);
-
-        river.render();
-        river.update();
     }
 }
